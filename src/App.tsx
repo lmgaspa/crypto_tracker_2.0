aa9da75eccc73d590b7fa76b3bb093a3d8f01c24
Luiz <seu-email@example.com>
2024-07-30 01:35:46 -0300

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { Container, Row, Col, Card, Form } from 'react-bootstrap';
+import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { PriceCoinsContext, PriceCoinsProvider, PriceCoinsContextProps } from './components/PriceCoins';
 import { PriceChangeContext, PriceChangeProvider, PriceChangeContextProps } from './components/PriceChange';
+import { FaSun, FaMoon } from 'react-icons/fa';
 import btc from './assets/images/btc.png';
 import eth from './assets/images/eth.png';
 import bnb from './assets/images/bnb.png';
@@ -16,9 +17,9 @@ import pepe from './assets/images/pepe.png';
 import trx from './assets/images/trx.png';
 import floki from './assets/images/floki.png';
 
-const coinImages = { 
+const coinImages = {
   BTC: btc, ETH: eth, BNB: bnb, SOL: sol, ADA: ada, XRP: xrp,
-  DOGE: doge, DOT: dot, SHIB: shib, PEPE: pepe, TRX: trx, FLOKI: floki 
+  DOGE: doge, DOT: dot, SHIB: shib, PEPE: pepe, TRX: trx, FLOKI: floki
 };
 
 const coinCategories = {
@@ -34,6 +35,7 @@ const App: React.FC = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [isTextVisible, setIsTextVisible] = useState(true);
   const [filter, setFilter] = useState<keyof typeof coinCategories>('ALL');
+  const [theme, setTheme] = useState('light');
 
   const currentCoin = coinCategories[filter][currentImageIndex];
   const currentCoinPrice = coinsPriceContext ? coinsPriceContext[`${currentCoin?.toLowerCase()}Price` as keyof PriceCoinsContextProps] : '0';
@@ -54,86 +56,84 @@ const App: React.FC = () => {
     setCurrentImageIndex(0);
   };
 
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
   if (!coinsPriceContext || !priceChangeContext) {
     return <div>Loading...</div>;
   }
 
   return (
-    <Container>
-      <h1 className="text-center my-4">CRYPTO TRACKER</h1>
-      <Form.Group controlId="filterSelect" style={{ paddingBottom: '20px' }}>
-        <Form.Label>Filter by Category</Form.Label>
-        <Form.Control as="select" value={filter} onChange={(event) => handleFilterChange(event as unknown as React.ChangeEvent<HTMLSelectElement>)}>
-          <option value="ALL">ALL</option>
-          <option value="MEME">MEME</option>
-          <option value="TOP">TOP</option>
-          <option value="UTILS">UTILS</option>
-        </Form.Control>
-      </Form.Group>
-      <Row>
-        {coinCategories[filter].map((coin) => {
-          const currentPrice = coinsPriceContext[`${coin.toLowerCase()}Price` as keyof PriceCoinsContextProps];
-          const priceChangePercent = priceChangeContext[`${coin.toLowerCase()}PriceChange` as keyof PriceChangeContextProps];
+    <div className={`d-flex justify-content-center align-items-center ${theme}`}>
+      <Container fluid className="w-75">
+        <h1 className="text-center my-4">
+          CRYPTO TRACKER
+          <Button onClick={toggleTheme} variant="link" className="theme-icon ml-3">
+            {theme === 'light' ? <FaSun size={24} /> : <FaMoon size={24} />}
+          </Button>
+        </h1>
+        <Form.Group controlId="filterSelect" className="pb-4">
+          <Form.Label>Filter by Category</Form.Label>
+          <Form.Control as="select" value={filter} onChange={(event) => handleFilterChange(event as unknown as React.ChangeEvent<HTMLSelectElement>)}>
+            <option value="ALL">ALL</option>
+            <option value="MEME">MEME</option>
+            <option value="TOP">TOP</option>
+            <option value="UTILS">UTILS</option>
+          </Form.Control>
+        </Form.Group>
+        <Row className="flex-grow-1">
+          {coinCategories[filter].map((coin) => {
+            const currentPrice = coinsPriceContext[`${coin.toLowerCase()}Price` as keyof PriceCoinsContextProps];
+            const priceChangePercent = priceChangeContext[`${coin.toLowerCase()}PriceChange` as keyof PriceChangeContextProps];
 
-          if (currentPrice === undefined || priceChangePercent === undefined) {
-            console.warn(`No data for ${coin}`);
-            return null;
-          }
+            if (currentPrice === undefined || priceChangePercent === undefined) {
+              console.warn(`No data for ${coin}`);
+              return null;
+            }
 
-          const priceColor = priceChangePercent >= 0 ? 'green' : 'red';
+            const priceColor = priceChangePercent >= 0 ? 'green' : 'red';
 
-          return (
-            <Col key={coin} sm={6} md={4} lg={3} className="mb-4">
-              <Card>
-                <Card.Body>
-                  <Card.Title className="d-flex align-items-center">
-                    <img src={coinImages[coin as keyof typeof coinImages]} alt={coin} style={{ width: '30px', marginRight: '10px' }} />
-                    <span>{coin}</span>
-                  </Card.Title>
-                  <div>
-                    <div className="d-flex justify-content-between">
-                      <span>Price:</span>
-                      <span style={{ color: priceColor }}>${currentPrice}</span>
+            return (
+              <Col key={coin} sm={6} md={4} lg={3} className="mb-4">
+                <Card>
+                  <Card.Body>
+                    <Card.Title className="d-flex align-items-center">
+                      <img src={coinImages[coin as keyof typeof coinImages]} alt={coin} style={{ width: '30px', marginRight: '10px' }} />
+                      <span>{coin}</span>
+                    </Card.Title>
+                    <div>
+                      <div className="d-flex justify-content-between">
+                        <span>Price:</span>
+                        <span style={{ color: priceColor }}>${currentPrice}</span>
+                      </div>
+                      <div className="d-flex justify-content-between">
+                        <span>24h %:</span>
+                        <span style={{ color: priceColor }}>
+                          {priceChangePercent > 0 ? '+' : ''}
+                          {priceChangePercent.toFixed(2)}%
+                        </span>
+                      </div>
                     </div>
-                    <div className="d-flex justify-content-between">
-                      <span>24h %:</span>
-                      <span style={{ color: priceColor }}>
-                        {priceChangePercent > 0 ? '+' : ''}
-                        {priceChangePercent.toFixed(2)}%
-                      </span>
-                    </div>
-                  </div>
-                </Card.Body>
-              </Card>
-            </Col>
-          );
-        })}
-      </Row>
-      <div
-        className="box"
-        style={{
-          textAlign: 'center',
-          marginTop: '2rem',
-          width: '100%',
-          height: '50px',
-          display: 'flex',
-          justifyContent: 'center',
-          padding: '40px',
-          alignItems: 'center',
-          background: 'linear-gradient(to left, #ffb1be, #9442ff)', // Inline gradient background
-        }}
-      >
-        {isTextVisible ? (
-          <>
-            <h1>{currentCoin} $ {currentCoinPrice}</h1>
-          </>
-        ) : (
-          <a href="http://www.dianaglobal.com.br" target="_blank" rel="noopener noreferrer" style={{ fontSize: '40px', color: 'rgb(47, 1, 61)', fontWeight: 'bold' }}>
-            visit www.dianaglobal.com.br
-          </a>
-        )}
-      </div>
-    </Container>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+        <div className="box d-flex justify-content-center align-items-center text-center mt-4 mb-4 p-4">
+          {isTextVisible ? (
+            <>
+              <h1 className="box-text">{currentCoin} $ {currentCoinPrice}</h1>
+            </>
+          ) : (
+            <a href="http://www.dianaglobal.com.br" target="_blank" rel="noopener noreferrer" className="box-link">
+              visit www.dianaglobal.com.br
+            </a>
+          )}
+        </div>
+      </Container>
+    </div>
   );
 }
 
